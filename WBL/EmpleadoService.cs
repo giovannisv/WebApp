58a392434583937c3c0a44ff5cfcfaf547b165be
giovannisv <geoseve@hotmail.com>
2021-10-14 04:26:44 -0600

    DesarrolloIndiana

---
+++ WBL/EmpleadoService.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BD;
+using Entity;
+
+namespace WBL
+{
+    public interface IEmpleadoService
+    {
+        Task<DBEntity> Create(EmpleadoEntity entity);
+        Task<DBEntity> Delete(EmpleadoEntity entity);
+        Task<IEnumerable<EmpleadoEntity>> Get();
+        Task<EmpleadoEntity> GetById(EmpleadoEntity entity);
+        Task<DBEntity> Update(EmpleadoEntity entity);
+    }
+
+    public class EmpleadoService : IEmpleadoService
+    {
+        private readonly IDataAccess sql;
+
+        public EmpleadoService(IDataAccess _sql)
+        {
+            sql = _sql;
+        }
+
+        #region Metodos Crud
+        //Metodo Get
+        public async Task<IEnumerable<EmpleadoEntity>> Get()
+        {
+            try
+            {
+                var result = sql.QueryAsync<EmpleadoEntity>("EmpleadoObtener");
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        //Metodo GetById
+        public async Task<EmpleadoEntity> GetById(EmpleadoEntity entity)
+        {
+            try
+            {
+                var result = sql.QueryFirstAsync<EmpleadoEntity>("EmpleadoObtener", new
+                { entity.IdEmleado });
+                return await result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        //Metodo create
+        public async Task<DBEntity> Create(EmpleadoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("EmpleadoInsertar", new
+                {
+                    entity.Nombre,
+                    entity.PrimerApellido,
+                    entity.SegundoApellido,
+                    entity.Edad,
+                    entity.FechaNacimiento
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<DBEntity> Update(EmpleadoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("EmpleadoActualizar", new
+                {
+                    entity.IdEmleado,
+                    entity.Nombre,
+                    entity.PrimerApellido,
+                    entity.SegundoApellido,
+                    entity.Edad,
+                    entity.FechaNacimiento
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<DBEntity> Delete(EmpleadoEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("EmpleadoEliminar", new
+                {
+                    entity.IdEmleado,
+
+                });
+                return await result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        #endregion
+
+
+
+
+
+
+
+    }
+}
