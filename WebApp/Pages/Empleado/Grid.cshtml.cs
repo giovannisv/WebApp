d08307c5c3aa9376c0591ec0fd051467246854e4
giovannisv <geoseve@hotmail.com>
2021-10-19 23:26:22 -0600

    actualizatodo

--- WebApp/Pages/Empleado/Grid.cshtml.cs
+++ WebApp/Pages/Empleado/Grid.cshtml.cs
@@ -4,13 +4,62 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WBL;
+using Entity;
 
 namespace WebApp.Pages.Empleado
 {
     public class GridModel : PageModel
     {
-        public void OnGet()
+        private readonly IEmpleadoService empleadoService;
+
+        public GridModel(IEmpleadoService empleadoService)
         {
+            this.empleadoService = empleadoService;
+        }
+
+        public IEnumerable<EmpleadoEntity> GridList { get; set; } = new List<EmpleadoEntity>();
+        public string Mensaje { get; set; } = "";
+
+        public async  Task<IActionResult> OnGet()
+        {
+            try
+            {
+                GridList = await empleadoService.Get();
+                if(TempData.ContainsKey("Msg"))
+                {
+                    Mensaje = TempData["Msg"] as string;
+                }
+                TempData.Clear();
+                return Page();
+
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }
+
+        }
+        public async Task<IActionResult> OnGetEliminar(int id)
+        {
+            try
+            {
+                var result  = await empleadoService.Delete(new() { IdEmleado=id});
+                if (result.CodeError !=0)
+                {
+                    throw new Exception(result.MsgError);
+                }
+                TempData["Msg"] = "Se elimino correctamente";
+                return Redirect("Grid");
+
+            }
+            catch (Exception ex)
+            {
+
+                return Content(ex.Message);
+            }
+
         }
     }
 }
